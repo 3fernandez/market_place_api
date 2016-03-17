b60174ce5e629c7c79ad8a6f8ef573b6852e58a0
Euclides Fernandes <ef.webdev@gmail.com>
2016-03-17 18:38:33 +0100

    Refactors test headers for each request.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,9 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  # concatenate the json format
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}" }
-  before(:each) { request.headers['Content-Type'] = Mime::JSON.to_s }
 
   describe "GET #show" do
     before(:each) do
