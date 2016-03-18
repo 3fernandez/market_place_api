9599a491a9f19956bfde7823b3b04d24e3671e03
Euclides Fernandes <ef.webdev@gmail.com>
2016-03-18 13:03:32 +0100

    Embeds the user into the product json output.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :title, :price, :published
+  # Include this, to pass user object to the product json response
+  has_one :user
 end
