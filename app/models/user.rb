604f012d5c3b24903c0c3d6ce52ca26761cea490
Euclides Fernandes <ef.webdev@gmail.com>
2016-03-18 20:43:41 +0100

    Adds user order has many relation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,11 +24,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
   validates :auth_token, uniqueness: true
 
   before_create :generate_authentication_token!
-
-  has_many :products, dependent: :destroy
 
   def generate_authentication_token!
     begin
